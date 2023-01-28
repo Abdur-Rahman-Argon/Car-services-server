ef03c8e9cea4b09a8aa259948712a0f7ad903bbf
Abdur-Rahman-Argon <abdurrahman.me7@gmail.com>
2023-01-29 00:53:46 +0600

    car server setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5002;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("genius car server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Genius Car server running on ${port}`);
+});
